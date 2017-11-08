50cf70e91f47e16918a0f47b02881b5720232d07
divyakul1 <dpkulkar@andrew.cmu.edu>
2017-11-08 14:54:50 -0500

    change to pose2D to be compatible with localizer node

--- scripts/local_planner.py
+++ scripts/local_planner.py
@@ -3,7 +3,7 @@ import rospy
 from geometry_msgs.msg  import Twist
 #from turtlesim.msg import Pose
 from std_msgs.msg import Float32
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose2D
 from math import pow,atan2,sqrt
 
 
@@ -13,10 +13,10 @@ class turtlebot():
         #Creating our node,publisher and subscriber
         rospy.init_node('local_planner', anonymous=True)
 	
-      	self.pub_lmotor = rospy.Publisher('/cabot/leftvel', Float32, queue_size=10)
-        self.pub_rmotor = rospy.Publisher('/cabot/rightvel', Float32, queue_size=10)
-        self.pose_subscriber = rospy.Subscriber('/cabot/pose', Pose, self.callback)
-        self.pose = Pose()
+      	self.pub_lmotor = rospy.Publisher('/leftvel', Float32, queue_size=10)
+        self.pub_rmotor = rospy.Publisher('/rightvel', Float32, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber('/pose', Pose2D, self.callback) #2d pose 
+        self.pose = Pose2D()
 
         self.rate = rospy.Rate(30)
 	self.w = rospy.get_param("~base_width", 0.1)
@@ -25,49 +25,42 @@ class turtlebot():
     #Callback function implementing the pose value received
     def callback(self, data):
         self.pose = data
-        self.pose.position.x = round(self.pose.position.x, 6)
-        self.pose.position.y = round(self.pose.position.y, 6)
+        self.pose.x = round(self.pose.position.x, 6)
+        self.pose.y = round(self.pose.position.y, 6)
 	
 
     def get_distance(self, goal_x, goal_y):
-        distance = sqrt(pow((goal_x - self.pose.position.x), 2) + pow((goal_y - self.pose.position.y), 2))
+        distance = sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2))
         return distance
 
     def move2goal(self):
-        goal_pose = Pose()
-	
-	gains = rospy.get_param('waypoints')
+        goal_pose = Pose2D()	
 	
 
-        goal_pose.position.x = 2
-        goal_pose.position.y = 3
+        goal_pose.x = 2
+        goal_pose.y = 3
         distance_tolerance = 0.00002
         vel_msg = Twist()
 	
 
 	#just added to hard code will need to remove once there is a topic to subsribe to that will continually update current pose 
 	
-	self.pose.position.x=self.pose.position.x
-	self.pose.position.y=self.pose.position.y
-	#self.pose.position.x=1 
-	#self.pose.position.y=2
-	self.pose.orientation.z=0
+
+	self.pose.x=1 
+	self.pose.y=2
+	self.pose.theta=0
 
 	#end hardcoded initial position
 
-       	while not rospy.is_shutdown() and sqrt(pow((goal_pose.position.x - self.pose.position.x), 2) + pow((goal_pose.position.y - self.pose.position.y), 2)) >= distance_tolerance:
+       	while not rospy.is_shutdown() and sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2)) >= distance_tolerance:
 
 		#Porportional Controller
 		#linear velocity in the x-axis:
-		linearx= 1.5* sqrt(pow((goal_pose.position.x - self.pose.position.x), 2) + pow((goal_pose.position.y - self.pose.position.y), 2))
-		#vel_msg.linear.y = 0
-		#vel_msg.linear.z = 0
+		linearx= 1.5* sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2))
 		    
 
 		#angular velocity in the z-axis:
-		#vel_msg.angular.x = 0
-		#vel_msg.angular.y = 0
-		angularz = 4 * (atan2(goal_pose.position.y - self.pose.position.y, goal_pose.position.x - self.pose.position.x) - self.pose.orientation.z)
+		angularz = 4 * (atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x) - self.pose.theta)
 		    
 		#Publishing left and right velocities
 

154882bd8725dd05ec2687802bdf52db9fd7ba84
divyakul1 <dpkulkar@andrew.cmu.edu>
2017-11-07 22:55:16 -0500

    changed equations to get right and left motor velocities based on paper

--- scripts/local_planner.py
+++ scripts/local_planner.py
@@ -6,6 +6,7 @@ from std_msgs.msg import Float32
 from geometry_msgs.msg import Pose
 from math import pow,atan2,sqrt
 
+
 class turtlebot():
 
     def __init__(self):
@@ -20,6 +21,7 @@ class turtlebot():
         self.rate = rospy.Rate(30)
 	self.w = rospy.get_param("~base_width", 0.1)
 
+
     #Callback function implementing the pose value received
     def callback(self, data):
         self.pose = data
@@ -33,16 +35,20 @@ class turtlebot():
 
     def move2goal(self):
         goal_pose = Pose()
-        goal_pose.position.x = 40.443421
-        goal_pose.position.y = -79.945592
+	
+	gains = rospy.get_param('waypoints')
+	
+
+        goal_pose.position.x = 2
+        goal_pose.position.y = 3
         distance_tolerance = 0.00002
         vel_msg = Twist()
 	
 
 	#just added to hard code will need to remove once there is a topic to subsribe to that will continually update current pose 
 	
-	self.pose.position.x=40.443428 
-	self.pose.position.y=-79.945562
+	self.pose.position.x=self.pose.position.x
+	self.pose.position.y=self.pose.position.y
 	#self.pose.position.x=1 
 	#self.pose.position.y=2
 	self.pose.orientation.z=0
@@ -53,8 +59,8 @@ class turtlebot():
 
 		#Porportional Controller
 		#linear velocity in the x-axis:
-		linearx= 1.5 * sqrt(pow((goal_pose.position.x - self.pose.position.x), 2) + pow((goal_pose.position.y - self.pose.position.y), 2))
-		#vel_msg.linear.y = 0sour
+		linearx= 1.5* sqrt(pow((goal_pose.position.x - self.pose.position.x), 2) + pow((goal_pose.position.y - self.pose.position.y), 2))
+		#vel_msg.linear.y = 0
 		#vel_msg.linear.z = 0
 		    
 
@@ -65,8 +71,8 @@ class turtlebot():
 		    
 		#Publishing left and right velocities
 
-		self.right = 1.0 * linearx + angularz * self.w / 2 
-		self.left = 1.0 * linearx - angularz * self.w / 2
+		self.right = angularz* linearx + self.w / 2 
+		self.left = angularz* linearx + self.w / 2
 		rospy.loginfo("publishing: (%f, %f)", self.right, self.left) 
 		        
 		self.pub_lmotor.publish(self.left)
@@ -76,7 +82,7 @@ class turtlebot():
 		self.rate.sleep()
 
       	#Stopping our robot after the movement is over
-	self.right = 0
+	self.right=0
 	self.left=0
 	self.pub_lmotor.publish(self.left)
 	self.pub_rmotor.publish(self.right)

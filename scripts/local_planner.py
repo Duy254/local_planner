1d2f8e9236b02172a641d52d5fe2f1ec6ba57132
andrewww2 <andrwww@utexas.edu>
2017-11-16 19:29:44 -0500

    Changes to local planner python
    Trying to have one program that can work with gazebo and real Cabot

--- scripts/local_planner.py
+++ scripts/local_planner.py
@@ -6,53 +6,78 @@ from std_msgs.msg import Float32
 from geometry_msgs.msg import Pose2D
 from math import pow, atan2, sqrt, acos, atan
 from arduino_msg.msg import Motor
-
-# from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry
 
 way_number = 1;
+realMode = "real"
+simMode = "simulation"
 
 #pose can either be assigned info from twist or a pose2D msg
 class Pose:
     x = 0
     y = 0
-    theta = 0
+    theta = 0       #pose specific (pose2D.theta)
+    quatW = 0       #odometry specific (Odometry.pose.pose.quaternion.w)
 
 class turtlebot():
+
     def __init__(self):
         # Creating our node,publisher and subscriber
         rospy.init_node('local_planner', anonymous=True)
 
-        self.pub_motor = rospy.Publisher('/motorSpeed', Motor, queue_size=10)
-
-        # subscribe to simulation instead need navmsg
-        # self.pose_subscriber=rospy.Subscriber('/odom', Odometry, self.callback) #Odom
-        self.pose_subscriber = rospy.Subscriber('pose', Pose2D, self.callback)
-
-        # add a publisher for gazebo
-        # self.pub_twist = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-
-        #self.pose = Pose()
-        self.pose = Pose2D()
-        # self.odom=Odometry()
+        #self.pose_subscriber = None
+        #self.pub_twist = None
+        self.pub_motor = rospy.Publisher('motorSpeed', Motor, queue_size=10)
+        self.pub_twist = rospy.Publisher('cmd_vel', Twist, queue_size = 10)  # add a publisher for gazebo
+        self.pose = Pose()
+        self.pose2D = Pose2D()
+        self.odom = Odometry()
 
         self.rate = rospy.Rate(10)
         self.mode = rospy.get_param("~mode", "real")
+
+        if self.mode == realMode:
+            self.pose_subscriber = rospy.Subscriber('pose', Pose2D, self.callback)
+
+        if self.mode == simMode:
+             # subscribe to simulation instead need navmsg
+            self.pose_subscriber = rospy.Subscriber('odom', Odometry, self.callback)
+
+            #self.pub_twist = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+
         self.w = rospy.get_param("~base_width", 0.2)
         self.distance_tolerance = rospy.get_param("~distance_tolerance", 0.002)
 
     # Callback function implementing the pose value received
     def callback(self, data):
-        # self.odom = data
-        self.pose = data
-        self.pose.x = round(self.pose.x, 6)
-        self.pose.y = round(self.pose.y, 6)
+        if (self.mode == realMode):
+            #self.pose2D = data
+            self.pose.x = round(self.pose2D.x, 6)
+            self.pose.y = round(self.pose2D.x, 6)
+            self.pose.theta = round(data.theta, 6)
 
-    # self.odom.pose.pose.position.x = round(self.odom.pose.pose.position.x, 6)
-    # self.odom.pose.pose.position.y = round(self.odom.pose.pose.position.y, 6)
+        if (self.mode == simMode):
+            #self.odom = data
+            self.pose.x = round(self.odom.pose.pose.position.x, 6)
+            self.pose.y = round(self.odom.pose.pose.position.y, 6)
+            self.pose.quatW = round(data.pose.pose.orientation.w, 6)
 
+    def pubMotors(self, linearX, angularZ):
+        goal_vel = Motor()
+        goal_twist = Twist()
+
+        rightVel = linearX + angularZ * self.w / 2
+        leftVel = linearX - angularZ * self.w / 2
+        goal_vel.left_speed = leftVel
+        goal_vel.right_speed = rightVel
+        self.pub_motor.publish(goal_vel)
+
+        # if (self.mode == simMode): #publish twist for gazebo simulation
+        goal_twist.linear.x = linearX
+        goal_twist.angular.z = angularZ
+        self.pub_twist.publish(goal_twist)
 
     def move2goal(self):
-
         global way_number
         p = rospy.get_param("/waypoints")
         strx = "/waypoints/1/x"
@@ -65,32 +90,35 @@ class turtlebot():
         goal_pose.x = rospy.get_param(strx)
         goal_pose.y = rospy.get_param(stry)
 
-        goal_vel = Motor()
-
-        # goal_twist=Twist()
+        # goal_vel = Motor()
+        # goal_twist = Twist()
 
         # while not rospy.is_shutdown() and sqrt(pow((goal_pose.x - self.odom.pose.pose.position.x), 2) + pow((goal_pose.y - self.odom.pose.pose.position.y), 2)) >= 0.05:
         while not rospy.is_shutdown() and sqrt(
                         pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2)) >= 0.05:
+
             # Porportional Controller
             # linear velocity in the x-axis:
             linearx = 0.1 * sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2))
             # linearx= 0.1* sqrt(pow((goal_pose.x - self.odom.pose.pose.position.x), 2) + pow((goal_pose.y - self.odom.pose.pose.position.y), 2))
 
-
             # angular velocity in the z-axis:
+            if (self.mode == simMode):
+                angularz = -0.8* (atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x) - acos(self.pose.quatW)*2) # quaternion to angle
 
-            # angularz = -0.8* (atan2(goal_pose.y - self.odom.pose.pose.position.y, goal_pose.x - self.odom.pose.pose.position.x) - acos(self.odom.pose.pose.orientation.w)*2) # quaternion to angle
+            if (self.mode == realMode):
+                angularz = -0.8 * (atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x) - self.pose.theta)
 
-            angularz = -0.8 * (atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x) - self.pose.theta)
+
             # Publishing left and right velocities
-            self.right = linearx + angularz * self.w / 2
-            self.left = linearx - angularz * self.w / 2
-
-            goal_vel.left_speed = self.left
-            goal_vel.right_speed = self.right
-            self.pub_motor.publish(goal_vel)
-
+            self.pubMotors(linearx, angularz)
+            # self.right = linearx + angularz * self.w / 2
+            # self.left = linearx - angularz * self.w / 2
+            #
+            # goal_vel.left_speed = self.left
+            # goal_vel.right_speed = self.right
+            # self.pub_motor.publish(goal_vel)
+            #
             # goal_twist.linear.x=linearx
             # goal_twist.angular.z=angularz
             # self.pub_twist.publish(goal_twist)
@@ -98,25 +126,25 @@ class turtlebot():
             self.rate.sleep()
 
         # Stopping our robot after the movement is over and no more waypoints to go to
-
         if (way_number >= len(p)):
-            self.right = 0
-            self.left = 0
+            self.pubMotors(0, 0)
 
-            goal_vel.left_speed = self.left
-            goal_vel.right_speed = self.right
-            self.pub_motor.publish(goal_vel)
-
-        # goal_twist.linear.x=0;
-        # goal_twist.angular.z=0;
-        # self.pub_twist.publish(goal_twist)
+            # self.right = 0
+            # self.left = 0
+            #
+            # goal_vel.left_speed = self.left
+            # goal_vel.right_speed = self.right
+            # self.pub_motor.publish(goal_vel)
+            #
+            # goal_twist.linear.x=0;
+            # goal_twist.angular.z=0;
+            # self.pub_twist.publish(goal_twist)
 
         else:
             way_number = way_number + 1
             turtlebot().move2goal()
 
         rospy.spin()
-
 
 if __name__ == '__main__':
     try:
